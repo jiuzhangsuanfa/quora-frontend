012aee420495d7da1204c49f090e74759eb98b96
Jiuzhangsuanfa <noreply@jiuzhang.com>
2020-10-15 08:48:16 +0800

    feat(suggestion): add suggestion component

---
+++ src/app/component/suggestion/suggestion.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-suggestion',
+  templateUrl: './suggestion.component.html',
+  styleUrls: ['./suggestion.component.scss']
+})
+export class SuggestionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() { }
+
+}
